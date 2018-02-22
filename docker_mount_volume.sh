2795ba0976385d1afd85b0ef1cfba5e7b59bf22a
Blake Jackson <blake.jackson@rtslabs.com>
2018-02-22 11:35:22 -0500

    added volume mounter convenience script

---
+++ docker_mount_volume.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ "$1" = "" ]
+then
+        echo "Please provide a source volume name"
+        exit
+fi
+
+if [ "$2" = "" ] 
+then
+        echo "Please provide a destination path name"
+        exit
+fi
+
+docker run -v $1:$2 -it --rm alpine /bin/ash
